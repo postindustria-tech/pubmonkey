fd09b93610ba363b7c010770cd88793d14185c40
mikhail.novikov <mikhail.novikov@postindustria.com>
2018-06-20 13:39:21 +0300

    fixed build script

--- scripts/build.js
+++ scripts/build.js
@@ -4,24 +4,33 @@ import { styles } from './styles'
 import { clear } from './clear'
 
 export function build(cb) {
-    gulp.series(bundle, styles, copyFiles)(cb)
+    gulp.series(clear, bundle, styles, copyFiles)(cb)
 }
 
-function copyFiles() {
-    copyFolder('images')
-    copyFolder('pages')
-    copyFolder('popup')
+function copyFiles(cb) {
+    gulp.series(
+        copyFolder('images'),
+        copyFolder('pages'),
+        copyFolder('popup'),
+        () => gulp.src([ 'src/manifest.json', 'src/index.html' ])
+            .pipe(gulp.dest('./build'))
+    )(cb)
+    // copyFolder('images')
+    // copyFolder('pages')
+    // copyFolder('popup')
 
-    return gulp.src([ 'src/manifest.json', 'src/index.html' ])
-        .pipe(gulp.dest('./build'))
+    // return gulp.src([ 'src/manifest.json', 'src/index.html' ])
+    //     .pipe(gulp.dest('./build'))
 }
 
 function copyFolder(folder) {
-    return gulp.src([
-            `src/${folder}/**/*`,
-            `!src/${folder}/**/*.js`,
-            `!src/**/*.old`,
-            `!src/**/*.scss`
-        ])
-        .pipe(gulp.dest(`./build/${folder}`))
+    return function() {
+        return gulp.src([
+                `src/${folder}/**/*`,
+                `!src/${folder}/**/*.js`,
+                `!src/**/*.old`,
+                `!src/**/*.scss`
+            ])
+            .pipe(gulp.dest(`./build/${folder}`))
+    }
 }
