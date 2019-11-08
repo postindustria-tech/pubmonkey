2be9fdaffeb7da9738eaa298da3153e26bb6e703
Alexander Burakovskiy <alexander.burakovskiy@gmail.com>
2019-11-08 12:38:00 +0200

    #148 - build number should auto-increment

--- scripts/incver.js
+++ scripts/incver.js
@@ -1,27 +1,36 @@
 import fs from 'fs'
-import { version } from '../package.json'
+import packageJson from '../package.json'
 
 export function incver(cb) {
-    let next = version.split('.')
+    let next = packageJson.version_name.split('.');
 
-    next[2]++
+    if (isNaN(next[3])) {
+        next[3] = 1;
+    } else {
+        next[3]++;
+    }
 
-    next = next.join('.')
+    const version_name = next.join('.');
+    const version = next.splice(0, 3).join('.');
 
-    ;[
-        './package.json',
-        './package-lock.json',
-        './src/manifest.json',
-        './src/misc.json'
-    ].forEach(filename => update(filename, next))
+    update('./package.json', 'version', version);
+    update('./package.json', 'version_name', version_name);
+
+    // update('./package-lock.json', 'version', version);
+
+    update('./src/manifest.json', 'version', version_name);
+
+    update('./src/misc.json', 'version', version_name);
 
     cb()
 }
 
-function update(filename, version) {
+function update(filename, field, value) {
+    const pattern = `"${field}"\\s*:\\s*"([^"]+)"`;
+    const searchValue = new RegExp(pattern,"g");
     fs.writeFileSync(
         filename,
         String(fs.readFileSync(filename))
-            .replace(/"version"\s*:\s*"([^"]+)"/, `"version": "${version}"`)
+            .replace(searchValue, `"${field}": "${value}"`)
     )
 }
