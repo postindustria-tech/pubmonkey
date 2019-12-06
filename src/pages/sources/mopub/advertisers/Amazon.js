a5852ba115733bdd546dc1b79ed2cf205bdf75df
Pavel Golman <pavel.golman@postindustria.com>
2019-12-06 11:58:12 +0200

    #197: No creavites created for Prebid order (160*600)

--- src/pages/sources/mopub/advertisers/Amazon.js
+++ src/pages/sources/mopub/advertisers/Amazon.js
@@ -9,7 +9,6 @@ export class Amazon extends AbstractAdvertiser {
         "320x50": "320 x 50 (Banner)",
         "300x250": "300 x 250 (MRect)",
         "728x90": "728 x 90 (Tablet Leaderboard)",
-        "160x600": "160 x 600 (Tablet Skyscraper)"
     };
 
     getCreativeHtmlData(params) {
