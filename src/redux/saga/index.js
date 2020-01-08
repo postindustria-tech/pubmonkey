3e60654f173ab24f6eab22b40602cf420fa73350
Pavel Golman <pavel.golman@postindustria.com>
2020-01-08 10:55:33 +0200

    #227: DFP. Duplicate order. Selected advertizer isn't saved

--- src/redux/saga/index.js
+++ src/redux/saga/index.js
@@ -222,8 +222,9 @@ function* setCreateOrderModalToggle(action) {
                                 rangeFrom = lineItem.bid;
                             }
                             adUnitKeys = [...adUnitKeys, ...lineItem.adUnitKeys].unique();
-                            isAmazon = Number.isInteger(lineItem.bid);
                         });
+
+                        isAmazon = Object.keys(order).includes('totalImpressionsDelivered')
                     }
 
                     if (isEmpty(values)) {
