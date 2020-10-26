d8f59c9ca0e0ca782ac2d03366858a1c301c43d3
Dmitriy Polikovskiy <dmitriypolikovskiy@MacBook-Pro-Dmitriy.local>
2020-10-26 14:11:35 +0200

    small refactoring

--- src/pages/sources/mopub/advertisers/Apollo.js
+++ src/pages/sources/mopub/advertisers/Apollo.js
@@ -22,8 +22,6 @@ export class Apollo extends AbstractAdvertiser {
             rangeTo,
             lineItemsNaming,
             granularity,
-            customEventClassName,
-            customEventData,
         } = params;
 
         let lineItemInfo = {...this.lineItemInfo},
